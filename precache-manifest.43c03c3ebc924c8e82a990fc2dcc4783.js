fb9e52ed1e1ef9d5e335a543d044bdb83ebd0437
Samuel Rowe <samuelrowe1999@gmail.com>
2020-07-29 13:30:10 +0530

    Initial commit

---
+++ precache-manifest.43c03c3ebc924c8e82a990fc2dcc4783.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7786f43a7bd8b9652dfcf8ff9f1c8a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda78edb8908b04eeb57",
+    "url": "/static/js/0.fa6f66b3.chunk.js"
+  },
+  {
+    "revision": "ded14259dadb6080fc47",
+    "url": "/static/js/1.bc7e32db.chunk.js"
+  },
+  {
+    "revision": "1c47b66ab3786cf2160d",
+    "url": "/static/js/10.a4133603.chunk.js"
+  },
+  {
+    "revision": "f8b4ca1c841a81c0c8c7",
+    "url": "/static/js/11.616e1c1f.chunk.js"
+  },
+  {
+    "revision": "9d453b8ab04e1a9b31cc",
+    "url": "/static/js/12.72cf4ae6.chunk.js"
+  },
+  {
+    "revision": "60b406e48736e7b74bc1",
+    "url": "/static/js/13.797f0bb2.chunk.js"
+  },
+  {
+    "revision": "18b52481796e6cff93a3",
+    "url": "/static/js/14.77deb1e7.chunk.js"
+  },
+  {
+    "revision": "b99ccb54dbca0c2712f4",
+    "url": "/static/js/15.f386202a.chunk.js"
+  },
+  {
+    "revision": "3e795875486b56a5f0dd",
+    "url": "/static/js/2.41270957.chunk.js"
+  },
+  {
+    "revision": "e51e23f7641486fe8b3b",
+    "url": "/static/js/5.9117d567.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/5.9117d567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e0f56a74d9e07e23f6",
+    "url": "/static/js/6.ee9e1e56.chunk.js"
+  },
+  {
+    "revision": "8bdb778ac4e50016767c",
+    "url": "/static/js/7.dd221ef2.chunk.js"
+  },
+  {
+    "revision": "66176f916abe3336d566",
+    "url": "/static/js/8.bb395dff.chunk.js"
+  },
+  {
+    "revision": "ace4f787102506a9e669",
+    "url": "/static/js/9.2165fef4.chunk.js"
+  },
+  {
+    "revision": "194331a5d2af08db4dee",
+    "url": "/static/js/main.9675d03c.chunk.js"
+  },
+  {
+    "revision": "23f4cda3906707f36a6f",
+    "url": "/static/js/runtime-main.396d6dbe.js"
+  },
+  {
+    "revision": "d90b3bff9221bde91893bbc7a5083756",
+    "url": "/static/media/Hubble.d90b3bff.ttf"
+  }
+]);
